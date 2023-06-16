1499768b57f1ec22c37142f3f872b1792d07331f
Kyle Runge <krunge1@hotmail.com>
2023-06-16 12:41:04 -0500

    minor adjustments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <Form/>
+      <div>
+        <h3>Your Form Data</h3>
+        <p>First Name: {person.firstName}</p>
+        <p>Last Name: {person.lastName}</p>
+        <p>Email: {person.email}</p>
+        <p>Password: {person.password}</p>
+        <p>Confirm Password: {person.confirmPassword}</p>
+    </div>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,13 @@ const Form = (person) => {
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
-    
+        const newPerson = {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+        }
     }
 
     const handleFirstName = (e) => {
@@ -132,14 +138,6 @@ const Form = (person) => {
                     </input>
                 </div>
             </form>
-            <div>
-                <h3>Your Form Data</h3>
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-                <p>Confirm Password: {confirmPassword}</p>
-            </div>
         </div>
     )
 }
